c709b8d9e6479c34ffb06fbfbe658324213b709e
invitaonline <invita.online@gmail.com>
2015-09-15 16:59:35 -0300

    outras classes

---
+++ app/src/main/java/br/com/invita/CadastroActivityFragment.java
@@ -0,0 +1,22 @@
+package br.com.invita;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class CadastroActivityFragment extends Fragment {
+
+    public CadastroActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cadastro, container, false);
+    }
+}
---
+++ app/src/main/java/br/com/invita/TipoEventoFragment.java
@@ -0,0 +1,22 @@
+package br.com.invita;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TipoEventoFragment extends Fragment {
+
+    public TipoEventoFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tipo_evento, container, false);
+    }
+}
